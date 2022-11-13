7341dd540cff986f8d1c20ab73a760e4befe6e9a
gabs-v <gabyvazquez8297@gmail.com>
2022-11-12 23:49:38 -0800

    components

---
+++ client/src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+const Home = () => {
+    return (
+        <div>
+            <div className='navegationBar'>
+                <h1> Gaby Vazquez </h1>
+                <ul>
+                    <li>About</li>
+                    <li>Projects</li>
+                    <li>Resume and References</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className='mainBody'>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Home
