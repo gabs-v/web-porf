39f8c4d1fd068e3d49f99b7af35a535f90a2fbd4
gabs-v <gabyvazquez8297@gmail.com>
2022-11-27 17:48:40 -0800

    front end edit

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -7,15 +7,15 @@ const Home = () => {
             <div className='header'>
                 <h1> Gaby Vazquez </h1>
                 <ul>
-                    <li>About</li>
-                    <li>Projects</li>
-                    <li>Resume and References</li>
+                    <li>Home</li>
+                    <li>About Me</li>
+                    <li>Skills and Projects</li>
                     <li>Contact</li>
                 </ul>
             </div>
             <div className='mainPage'>
                 <div className='leftArea'>
-
+                    <image className='image'/>
                 </div>
                 <div className='rightArea'>
                     Hi, I'm <br/> Gaby Vazquez. <br/>
